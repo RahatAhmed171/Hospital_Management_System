e87a6b4c6aae694fd9be319a222afbf28967d67e
RahatAhmed171 <69518859+RahatAhmed171@users.noreply.github.com>
2023-09-08 19:38:41 +0600

    Update inpatient_reg.php

--- inpatient_reg.php
+++ inpatient_reg.php
@@ -1,3 +1,23 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital_management";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  echo "Connected unsuccessfully";
+  die("Connection failed: " . mysqli_connect_error());
+}
+else{
+
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -69,7 +89,7 @@ footer {
   <h1>Inpatient Registration</h1>
   </main>
 
-  <form id="form-user" action="#" method="post">
+  <form id="form-user" action="inpatient_reg.php" method="post">
         
     <div class="form-box" data-errormsg="">
         <label for="input-name">Name:</label>
@@ -94,25 +114,36 @@ footer {
 
     <div class="form-box" data-errormsg="">
       <label for="input-hproblem">Health Problem:</label>
-      <input type="text" id="input-hproblem" autofocus placeholder="Health Problem" name="Health Problem" tabindex="1"/>
+      <input type="text" id="input-hproblem" autofocus placeholder="Health Problem" name="Health_Problem" tabindex="1"/>
   </div>
 
   <div class="form-box" data-errormsg="">
     <label for="input-sdate">Starting Date:</label>
-    <input type="date" id="input-sdate" autofocus placeholder="Starting Date" name="Starting Date" tabindex="1"/>
+    <input type="date" id="input-sdate" autofocus placeholder="Starting Date" name="Starting_Date" tabindex="1"/>
 </div>
 
 <div class="form-box" data-errormsg="">
-  <label for="bed-toggle">Bed No:</label>
-  <input type="checkbox" id="bed-toggle" autofocus placeholder="Bed No" name="Bed No" tabindex="1"/>
-  <ul id="bed-choices">
-    <li>101</li>
-    <li>102</li>
-    <li>103</li>
-    <li>104</li>
-    <li>105</li>
-</ul>
+<label for="input-room">Room No:</label>
+        <input type="number" id="input-room" autofocus placeholder="Room" name="Room" tabindex="1"/>
 </div>
+<label for="doctor-assigned">Doctor Assigned:</label>
+      
+      <ul id="doctor-list" style="display: none;">
+      <?php
+          $query = "SELECT docid,name FROM doctor";
+          $result = mysqli_query($conn, $query);
+     
+          if (mysqli_num_rows($result) > 0) {
+              // 3. Loop through the results and generate the dropdown options
+              while ($row = mysqli_fetch_assoc($result)) {
+                  $doctorName = $row["name"];
+                  echo "<li onclick='selectDoctor(\"$doctorName\")'>$doctorName</li>";
+              }
+          }
+       ?>
+      </ul>
+  
+  <input type="text" id="doctor-assigned" name="doctor-assigned">
   
     <div class="form-box">
         <button id="button-reset">Cancel</button>
@@ -121,5 +152,76 @@ footer {
 </form>
 
   <footer> Address: Probortok Circle, Chittagong</footer>
+<script>
+   var dropdownOpen = false;
+        const doctorAssignedInput = document.getElementById('doctor-assigned');
+        doctorAssignedInput.addEventListener('click', function() {
+   toggleDropdown();
+});
+        function toggleDropdown() {
+            var doctorList = document.getElementById("doctor-list");
+            dropdownOpen = !dropdownOpen;
+
+            if (dropdownOpen) {
+                doctorList.style.display = "block";
+            } else {
+                doctorList.style.display = "none";
+            }
+        }
+
+        function selectDoctor(doctorName) {
+          
+            document.getElementById("doctor-assigned").value = doctorName;
+            toggleDropdown();
+        }
+  </script>
 </body>
 </html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+  $name = $_POST["Name"];
+  $phone = $_POST["Phone"];
+  $address = $_POST["Address"];
+  $age = $_POST["Age"];
+  $gender = $_POST["Gender"];
+  $problem = $_POST["Health_Problem"];
+  $start = $_POST["Starting_Date"];
+  $room = $_POST["Room"];
+  $doctorAssigned = $_POST["doctor-assigned"];
+
+  
+  $query = "SELECT docid,name FROM doctor";
+  $result = mysqli_query($conn, $query);
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+     
+      $doctorName = $row['name'];
+      
+      if ($doctorAssigned === $doctorName) {
+          // Found a match, $doctorID now contains the ID of the selected doctor
+          $doctorAssignedID = $row['docid'];
+          break; // Exit the loop since we found the ID
+      }
+      
+  }
+
+
+
+
+
+  $sql = "INSERT INTO inpatient (name,age,phone,sex,address,health_problem,start_date,end_date,bed_no,docid,room_id) VALUES ('$name',$age,'$phone','$gender','$address','$problem','$start',NULL,NULL,$doctorAssignedID,$room)";
+if ($conn->query($sql) === TRUE) {
+  echo "outpatient created successfully";
+
+
+  // Redirect to the success page with the ID as a query parameter
+  header("Location: index.php");
+  exit();
+}
+ else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+}
+?>
