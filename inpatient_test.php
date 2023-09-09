a7ec9b15495961cf152b454ab59f20a65abb9d54
RahatAhmed171 <69518859+RahatAhmed171@users.noreply.github.com>
2023-09-09 12:48:18 +0600

    Update inpatient_test.php

--- inpatient_test.php
+++ inpatient_test.php
@@ -1,3 +1,22 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital_management";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  echo "Connected unsuccessfully";
+  die("Connection failed: " . mysqli_connect_error());
+}
+else{
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -23,12 +42,12 @@ header {
 }
 main {
   /* header and footer padding */
-  padding-top: 60px;
-  padding-bottom: 60px;
+  padding-top: 30px;
+  padding-bottom: 20px;
 }
 main>h1{
     text-align: center;
-    font-size: 30px;
+    font-size: 20px;
 }
 
 
@@ -53,12 +72,15 @@ footer {
    <h1>Inpatient Test Registration </h1>
     </main>
     
+    
+    <form action="inpatient_test.php" method="POST">
+   
+    <div class="form-one-container">
     <label for="inpatient-id">Inpatient ID:</label>
     <input type="text" id="inpatient-id" name="inpatient-id">
-    <div class="form-container">
-    <form>
-    <label for="test-name">Test name:</label>
-    <select id="test-name" name="test-name" size="10">
+    <br>
+    <label for="test-one-name">Test name:</label>
+    <select id="test-one-name" name="test-one-name" size="10">
     <option value="X-Ray">X-Ray</option>
     <option value="Blood Count">Blood Count (CBC)</option>
     <option value="MRI">MRI</option>
@@ -72,28 +94,39 @@ footer {
     <!-- Add more test options as needed -->
     </select>
     
-    <label for="test-amount">Test amount:</label>
-    <input type="number" id="test-amount" name="test-amount">
+    <label for="test-one-amount">Test amount:</label>
+    <input type="number" id="test-one-amount" name="test-one-amount">
     
-    <label for="test-date">Test Date:</label>
-    <input type="date" id="test-date" name="test-date">
+    <label for="test-one-date">Test Date:</label>
+    <input type="date" id="test-one-date" name="test-one-date">
     
-    <label for="doctor-name">Doctor name:</label>
-    <select id="doctor-name" name="doctor-name" size="10">
-    <option value="doc1">Doctor 1</option>
-    <option value="doc2">Doctor 2</option>
-    <option value="doc3">Doctor 3</option>
-    <!-- Add more doctor options as needed -->
-    </select>
+    <label for="doctor-one-name">Doctor name:</label>
+       
+            <ul id="doctor-one-list" style="display: none;">
+             <?php
+             ob_start();
+                $query = "SELECT docid,name FROM doctor";
+                $result = mysqli_query($conn, $query);
+           
+                if (mysqli_num_rows($result) > 0) {
+                    // 3. Loop through the results and generate the dropdown options
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $doctorName = $row["name"];
+                        
+                        echo "<li onclick='selectoneDoctor(\"$doctorName\")'>$doctorName</li>";
+                    }
+                }
+             ?>
+            </ul>
+        
+        <input type="text" id="doctor-one-name" name="doctor-one-name">
     
-    
-    </form>
     </div>
     <br>
-    <div class="form-container">
-    <form>
-    <label for="test-name">Test name:</label>
-    <select id="test-name" name="test-name" size="10">
+    <div class="form-two-container">
+    
+    <label for="test-two-name">Test name:</label>
+    <select id="test-two-name" name="test-two-name" size="10">
     <option value="X-Ray">X-Ray</option>
     <option value="Blood Count">Blood Count (CBC)</option>
     <option value="MRI">MRI</option>
@@ -104,26 +137,39 @@ footer {
     <option value="HIV">HIV</option>
     <option value="Prostate Test">Prostate Test</option>
     <option value="Dengue Test">Dengue Test</option>
+    
     <!-- Add more test options as needed -->
     </select>
     
-    <label for="test-amount">Test amount:</label>
-    <input type="number" id="test-amount" name="test-amount">
+    <label for="test-two-amount">Test amount:</label>
+    <input type="number" id="test-two-amount" name="test-two-amount">
     
-    <label for="test-date">Test Date:</label>
-    <input type="date" id="test-date" name="test-date">
+    <label for="test-two-date">Test Date:</label>
+    <input type="date" id="test-two-date" name="test-two-date">
     
-    <label for="doctor-name">Doctor name:</label>
-    <select id="doctor-name" name="doctor-name" size="10">
-    <option value="doc1">Doctor 1</option>
-    <option value="doc2">Doctor 2</option>
-    <option value="doc3">Doctor 3</option>
-    <!-- Add more doctor options as needed -->
-    </select>
-    
-    </form>
+    <label for="doctor-two-name">Doctor name:</label>
+      
+            <ul id="doctor-list" style="display: none;">
+            <?php
+                $query = "SELECT docid,name FROM doctor";
+                $result = mysqli_query($conn, $query);
+           
+                if (mysqli_num_rows($result) > 0) {
+                    // 3. Loop through the results and generate the dropdown options
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $doctorName = $row["name"];
+                        echo "<li onclick='selectDoctor(\"$doctorName\")'>$doctorName</li>";
+                    }
+                }
+             ?>
+            </ul>
+        
+        <input type="text" id="doctor-two-name" name="doctor-two-name">
     </div>
-    <button>Add test</button>
+    <button>Register Test</button>
+    </form>
+    
+   
     
     <!-- Repeat the above div structure for the other two forms -->
     
@@ -134,8 +180,138 @@ footer {
 
         
         
-    </body>
+    
 
     <footer> Address: Probortok Circle, Chittagong</footer>
+    <script>
+        var dropdownOpen = false;
+        const doctorTwoNameInput = document.getElementById('doctor-two-name');
+        doctorTwoNameInput.addEventListener('click', function() {
+   toggleDropdown();
+});
+        function toggleDropdown() {
+            var doctorList = document.getElementById("doctor-list");
+            dropdownOpen = !dropdownOpen;
+
+            if (dropdownOpen) {
+                doctorList.style.display = "block";
+            } else {
+                doctorList.style.display = "none";
+            }
+        }
+
+        function selectDoctor(doctorName) {
+          
+            document.getElementById("doctor-two-name").value = doctorName;
+            toggleDropdown();
+        }
+
+        const doctorOneNameInput = document.getElementById('doctor-one-name');
+        doctorOneNameInput.addEventListener('click', function() {
+   toggleoneDropdown();
+});
+        function toggleoneDropdown() {
+            var doctorList = document.getElementById("doctor-one-list");
+            dropdownOpen = !dropdownOpen;
+
+            if (dropdownOpen) {
+                doctorList.style.display = "block";
+            } else {
+                doctorList.style.display = "none";
+            }
+        }
+
+        function selectoneDoctor(doctorName) {
+          //document.getElementById("selected-one-doctor").textContent = doctorName;
+            document.getElementById("doctor-one-name").value = doctorName;
+            toggleoneDropdown();
+        }
+    </script>
+
 </body>
+
 </html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+//thik korte hbe
+$IPid=$_POST["inpatient-id"];
+
+$test_one_name = $_POST["test-one-name"];
+$test_one_amount = $_POST["test-one-amount"];
+$test_one_date = $_POST["test-one-date"];
+$doctorOneName = $_POST["doctor-one-name"];
+
+$test_two_name = $_POST["test-two-name"];
+$test_two_amount = $_POST["test-two-amount"];
+$test_two_date = $_POST["test-two-date"];
+$doctorTwoName = $_POST["doctor-two-name"];
+
+
+
+
+
+// Insert data into the database
+
+$query = "SELECT docid,name FROM doctor";
+$result = mysqli_query($conn, $query);
+  
+  while ($row = mysqli_fetch_assoc($result)) {
+   
+    $doctorName = $row['name'];
+    
+    if ($doctorOneName === $doctorName) {
+        // Found a match, $doctorID now contains the ID of the selected doctor
+        $doctorOneID = $row['docid'];
+        break; // Exit the loop since we found the ID
+    }
+    
+}
+$query = "SELECT docid,name FROM doctor";
+$result = mysqli_query($conn, $query);
+while ($row2 = mysqli_fetch_assoc($result)) {
+  
+  $doctorName = $row2['name'];
+  
+  if ($doctorTwoName === $doctorName) {
+      // Found a match, $doctorID now contains the ID of the selected doctor
+      $doctorTwoID = $row2['docid'];
+      break; // Exit the loop since we found the ID
+  }
+ 
+}
+
+$sql1 = "INSERT INTO test (patient_type,test_name,test_amount,test_date,test_result,docid,opid,ipid	) VALUES ('inpatient','$test_one_name',$test_one_amount,'$test_one_date','null','$doctorOneID',NULL,'$IPid')";
+$sql2 = "INSERT INTO test (patient_type,test_name,test_amount,test_date,test_result,docid,opid,ipid	) VALUES ('inpatient','$test_two_name',$test_two_amount,'$test_two_date','null','$doctorTwoID',NULL,'$IPid')";
+
+if ($conn->query($sql1) === TRUE) {
+ 
+ 
+
+  // Redirect to the success page with the ID as a query parameter
+
+} else {
+  echo "Error: " . $sql1 . "<br>" . $conn->error;
+}
+if ($conn->query($sql2) === TRUE) {
+ 
+  
+  $output=ob_get_clean();
+  header("Location: index.php");
+  exit();
+
+  // Redirect to the success page with the ID as a query parameter
+  
+
+} else {
+  echo "Error: " . $sql2 . "<br>" . $conn->error;
+}
+
+
+
+// Insert data into the database
+
+
+$conn->close();
+}
+?>
