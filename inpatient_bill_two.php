7ffd9e582acd6cd21efd27f61797a6f9a2f95229
RahatAhmed171 <69518859+RahatAhmed171@users.noreply.github.com>
2023-09-11 13:55:05 +0600

    Update inpatient_bill_two.php

--- inpatient_bill_two.php
+++ inpatient_bill_two.php
@@ -6,7 +6,7 @@ $password = "";
 $dbname = "hospital_management";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+ob_start();
 // Check connection
 if (!$conn) {
   echo "Connected unsuccessfully";
@@ -16,59 +16,84 @@ else{
 echo "Connected successfully";
 }
 // kaj hocche start date-end date calculate, test charges calculate and insert to bill
-$outpatientID=$_GET['data1'];
-echo "$outpatientID";
-$testFirstID=$_GET['inserted_first_id'];
-$testSecondID=$_GET['inserted_second_id'];
-$sql1="SELECT name,age,phone,sex FROM outpatient where opid=$outpatientID";
+$inpatientID=$_GET['inpatient_id'];
+$surgeries=$_GET['surgery'];
+$sql1="SELECT start_date, end_date, DATEDIFF(end_date, start_date) AS day_difference
+FROM inpatient where ipid = '$inpatientID'";
 $result1 = $conn->query($sql1);
+
+
 
 if ($result1) {
     $row1 = $result1->fetch_assoc();
-    $patient_name = $row1['name'];
-    $patient_age = $row1['age'];
-    $patient_phone = $row1['phone'];
-    $patient_sex = $row1['sex'];
-    $result1->free();
+    $dayDifference = $row1["day_difference"];
+    $roomcharge=$dayDifference*1000;
+  
 } else {
     // Handle any errors with the query.
     echo "Error in query 1: " . $conn->error;
 }
-$sql2="SELECT test_name,test_amount FROM test where test_id=$testFirstID";
+$sql2="SELECT SUM(test_amount) AS total_amount FROM test WHERE ipid ='$inpatientID'";
 $result2 = $conn->query($sql2);
 
 if ($result2) {
     $row2 = $result2->fetch_assoc();
-    $testName1 = $row2['test_name'];
-    $testAmount1 = $row2['test_amount'];
-    $result2->free();
-} else {
+    if ($row2['total_amount'] !== null) {
+      // If rows were returned, set the PHP variable
+      $totalTestCharges = $row2['total_amount'];
+  } else {
+      // If no rows were returned, set the PHP variable to zero
+      $totalTestCharges = 0;
+  }
+
+}
+ else {
     // Handle any errors with the query.
     echo "Error in query 1: " . $conn->error;
 }
-$sql3="SELECT test_name,test_amount FROM test where test_id=$testSecondID";
+$grandTotal=$totalTestCharges+$roomcharge+$surgeries;
+
+
+$sql3="INSERT INTO bill (test_charge,no_of_days,room_charge,Surgeries,Total,ipid,opid	) VALUES ('$totalTestCharges','$dayDifference',$roomcharge,'$surgeries','$grandTotal','$inpatientID',NULL)";
 $result3 = $conn->query($sql3);
 
 if ($result3) {
-    $row3 = $result3->fetch_assoc();
-    $testName2 = $row3['test_name'];
-    $testAmount2 = $row3['test_amount'];
-    $result3->free();
+   
+   
 } else {
     // Handle any errors with the query.
     echo "Error in query 2: " . $conn->error;
 }
-
+$sql4="SELECT name,age,phone,sex,address,start_date,end_date,room_id from inpatient WHERE ipid ='$inpatientID'";
+$result4 = $conn->query($sql4);
 // Calculate the total amount
-$totalAmount = $testAmount1 + $testAmount2;
+if ($result4) {
+  $row4 = $result4->fetch_assoc();
 
+  // Store each column's value in separate variables
+  $name = $row4['name'];
+  $age = $row4['age'];
+  $phone = $row4['phone'];
+  $sex = $row4['sex'];
+  $address = $row4['address'];
+  $start_date = $row4['start_date'];
+  $end_date = $row4['end_date'];
+  $room_id = $row4['room_id'];
+
+
+} else {
+  // Handle any errors with the query.
+  echo "Error in query 2: " . $conn->error;
+}
+$output=ob_get_clean();
+$conn->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
-    <title>Outpatient Bill </title>
+    <title>Inpatient Bill </title>
     <link rel="icon" type="image/png" href="http://example.com/myicon.png">
     <style>
 body {
@@ -122,55 +147,55 @@ footer {
         
         <div class="form-box" data-errormsg="">
             <label for="input-name">Name:</label>
-            <input type="text" id="input-name" required placeholder="Name" name="Name" tabindex="2" />
+            <input type="text" id="input-name" value="<?php echo $name; ?>"  readonly><br>
         </div>
         <div class="form-box" data-errormsg="">
             <label for="input-phone">Phone:</label>
-            <input type="text" id="input-phone" autofocus placeholder="Phone" name="Phone" tabindex="1"/>
+            <input type="text" id="input-phone" value="<?php echo $phone; ?>"  readonly><br>
         </div>
         <div class="form-box" data-errormsg="">
             <label for="input-address">Address:</label>
-            <input type="text" id="input-address" autofocus placeholder="Address" name="Address" tabindex="2" />
+            <input type="text" id="input-address" value="<?php echo $address; ?>"  readonly><br>
         </div>
         <div class="form-box" data-errormsg="">
             <label for="input-age">Age:</label>
-            <input type="number" id="input-number" autofocus placeholder="Number" name="Age" tabindex="1"/>
+            <input type="number" id="input-number" value="<?php echo $age; ?>"  readonly><br>
         </div>
         <div class="form-box" data-errormsg="">
             <label for="input-gender">Gender:</label>
-            <input type="text" id="input-gender" autofocus placeholder="Gender" name="Gender" tabindex="1"/>
+            <input type="text" id="input-gender" value="<?php echo $sex; ?>"  readonly><br>
         </div>
 
     
       <div class="form-box" data-errormsg="">
         <label for="input-sdate">Starting Date:</label>
-        <input type="date" id="input-sdate" autofocus placeholder="Starting Date" name="Starting_Date" tabindex="1"/>
+        <input type="date" id="input-sdate" value="<?php echo $start_date; ?>"  readonly><br>
     </div>
     <div class="form-box" data-errormsg="">
         <label for="input-edate">Ending Date:</label>
-        <input type="date" id="input-edate" autofocus placeholder="Ending Date" name="Ending_Date" tabindex="1"/>
+        <input type="date" id="input-edate" value="<?php echo $end_date; ?>"  readonly><br>
     </div>
     
     <div class="form-box" data-errormsg="">
     <label for="input-room">Room No:</label>
-            <input type="number" id="input-room" autofocus placeholder="Room" name="Room" tabindex="1"/>
+            <input type="number" id="input-room" value="<?php echo $room_id; ?>"  readonly><br>
     </div>
 
     <div class="form-box" data-errormsg="">
     <label for="input-tcharge">Test Charges:</label>
-            <input type="number" id="input-tcharge" autofocus placeholder="Test Charges" name="tcharges" tabindex="1"/>
+            <input type="number" id="input-tcharge" value="<?php echo $totalTestCharges; ?>"  readonly><br>
     </div>
     <div class="form-box" data-errormsg="">
     <label for="input-rcharge">Room Charges:</label>
-            <input type="number" id="input-rcharge" autofocus placeholder="Room Charges" name="rcharges" tabindex="1"/>
+            <input type="number" id="input-rcharge" value="<?php echo $roomcharge; ?>"  readonly><br>
     </div>
     <div class="form-box" data-errormsg="">
     <label for="input-scost">Surgery Costs:</label>
-            <input type="number" id="input-scost" autofocus placeholder="Surgery Costs" name="scosts" tabindex="1"/>
+            <input type="number" id="input-scost" value="<?php echo $surgeries; ?>"  readonly><br>
     </div>
     <div class="form-box" data-errormsg="">
     <label for="input-total">Total:</label>
-            <input type="number" id="input-total" autofocus placeholder="Total" name="total" tabindex="1"/>
+            <input type="number" id="input-total" value="<?php echo $grandTotal; ?>"  readonly><br>
     </div>
 </form>
   
